46669474b7e6bc35ddefe23250733634c3b59b5e
pandreea14 <99077035+pandreea14@users.noreply.github.com>
2023-02-25 19:46:19 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,16 @@
 #include <stdlib.h>
 using namespace std;
 
+bool isPrime(int n)
+{
+	if (n < 2) return false;
+	for (int tr = 2; tr <= n / 2; tr++)
+		if ((n % tr) == 0)
+			return false;
+	return true;
+
+}
+
 int main()
 {
 	//Exemplificare printf
@@ -20,8 +30,9 @@ int main()
 	printf("\n\n");
 
 
-	printf("-----------------[EXEMPLU PROBLEMA 1]------------------------\n");
+	printf("-----------------[PROBLEMA 1]------------------------\n");
 	FILE* fp;
+	int index, nr, suma = 0;
 	if (fopen_s(&fp, "in.txt", "r") != 0)
 	{
 		printf("Eroare. Nu am putut deschide fisierul\n");
@@ -35,34 +46,59 @@ int main()
 			printf("Am citit din fisier: %s\n", myString);
 		}
 		*/
-		while (fgets(myString, 200, fp))
+		
+		while (fgets(myString, 200, fp))//citeste un rand din fisier si il pune in charul myString
 		{
-			myString[strlen(myString) - 1] = '\0';
+			myString[strlen(myString)-1] = '\0';
 			printf("Am citit din fisier: %s\n", myString);
+
+			nr = 0, index = 0;
+			while (index < strlen(myString))
+			{
+				nr = nr*10 + (myString[index] - '0');
+				index++;
+			}
+			printf("-Nr meu este %d\n\n", nr);
+
+			suma += nr;
 		}
+	}
+	printf("Suma numerelor din fisier este %d\n", suma);
+	printf("-----------------------------------------------------");
+	printf("\n\n");
+
+	printf("-----------------[PROBLEMA 2]------------------------\n");
+	
+	printf("Introduceti o propozitie: ");
+	char vectCuvinte[50][200], sentence[200];
+	scanf_s("%200s", sentence);
+	//scanf("%19s", cuvant);
+	int counter = 0, numarCuvinte = 0;
+	while (scanf_s("%200s", vectCuvinte[counter]))
+	{
+		//if (strcmp(cuvant, "\n\n") == 0)
+		//	break;
+
+		numarCuvinte++;
+
+		printf("Cuvantul citit este: %s\n", vectCuvinte[counter]);
+		counter++;
+
+		//if (counter >= numarCuvinte)
+			//break;
 	}
 	printf("-----------------------------------------------------");
 	printf("\n\n");
 
-	printf("-----------------[EXEMPLU PROBLEMA 2]------------------------\n");
-	printf("Introduceti numarul de cuvinte din propozitie: ");
-	int numarCuvinte;
-	scanf_s("%d", &numarCuvinte, 4);
-	printf("Introduceti o propozitie: ");
-	char cuvant[20];
-	//scanf("%19s", cuvant);
-	int counter = 0;
-	while (scanf_s("%19s", cuvant, 19))
-	{
-		//if (strcmp(cuvant, "\n\n") == 0)
-		//	break;
-		printf("Cuvantul citit este: %s\n", cuvant);
-		counter += 1;
-		if (counter >= numarCuvinte)
-			break;
-	}
-	printf("-----------------------------------------------------");
-	printf("\n\n");
+	printf("-----------------[PROBLEMA 3]------------------------\n");
+	int n;
+	cout << "Enter a number:";
+	cin >> n;
+
+	if (isPrime(n))
+		cout << n << " is prime !";
+	else
+		cout << n << " is NOT prime !";
 
 	system("pause");
 	return 0;
